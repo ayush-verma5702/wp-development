ee821225c6f0826d8af29fbafc17b37b70734211
ayush-verma5702 <ayush.verma@altudo.co>
2023-09-22 17:16:12 +0530

    New Commit-Plugin-added queries and sc

--- wp-content/plugins/my-plugin/my-plugin.php
+++ wp-content/plugins/my-plugin/my-plugin.php
@@ -13,7 +13,24 @@
 
  function my_plugin_activation()
  {
-   //
+   global $wpdb, $table_prefix;
+   $wp_emp = $table_prefix.'emp';
+
+   $q= "CREATE TABLE IF NOT EXISTS `$wp_emp` (`ID` INT NOT NULL , `name` VARCHAR(50) NOT NULL ,
+    `email` VARCHAR(50) NOT NULL , `status` BOOLEAN NOT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB;";
+
+   $wpdb->query($q);
+
+   //$q= "INSERT INTO `$wp_emp` (`name`, `email`, `status`) VALUES ('Ayush', 'ayus@xyz.com', 1);";
+
+   $data = array(
+    'name' => 'Ayush',
+    'email' => 'ayush@xyz.com',
+    'status' => 1
+   );
+
+   $wpdb->insert($wp_emp, $data);
+
  }
 
  
@@ -21,12 +38,17 @@
 
  function my_plugin_deactivation()
  {
-    // 
+    global $wpdb, $table_prefix;
+    $wp_emp = $table_prefix.'emp';
+
+    $q = "TRUNCATE `$wp_emp`";
+    $wpdb->query($q);
  }
 
  register_deactivation_hook(__FILE__, 'my_plugin_deactivation');
 
  function my_sc_fun(){
-    return 'ShortCode Example';
+
+    include 'sc_ex.php';
  }
  add_shortcode('my-sc','my_sc_fun');
--- wp-content/plugins/my-plugin/uninstall.php
+++ wp-content/plugins/my-plugin/uninstall.php
@@ -4,3 +4,11 @@ if(!defined('WP_UNINSTALL_PLUGIN')){
     header("Location:/Wordpress");
     die('');
 }
+
+global $wpdb, $table_prefix;
+
+$wp_emp = $table_prefix.'emp';
+
+$q = "DROP TABLE `$wp_emp`;";
+
+$wpdb->query($q);
